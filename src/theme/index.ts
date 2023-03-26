f20e0c18f4f30661cb9835c38c73f3ea674f596f
Drew Hund <languageinvestigator@gmail.com>
2023-03-26 15:54:49 -0400

    navbar styled and social links added

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -6,18 +6,18 @@ const config: ThemeConfig = {
 }
 
 export const theme = extendTheme({
-  config,
+  ...config,
   colors: {
     brand: {
       bodyText: "#A0030B",
       mainBg: "#48483A",
-      secondaryBg: "5C574F",
+      secondaryBg: "#5C574F",
       headers: "#DEC102",
       subtitles: "#C5D1EB"
     }
   },
   fonts: {
-    heading: `Roboto Mono, '${base.fonts?.heading}`,
+    heading: `Roboto Mono, ${base.fonts?.heading}`,
     body: `Raleway, ${base.fonts?.body}`
   }
 })
