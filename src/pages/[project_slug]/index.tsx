9072783cba48a35ea75a412b829f15ed44a3159d
Drew Hund <languageinvestigator@gmail.com>
2023-05-07 17:17:30 -0400

    fallback if project title not there

--- src/pages/[project_slug]/index.tsx
+++ src/pages/[project_slug]/index.tsx
@@ -7,7 +7,7 @@ const ProjectShow = () => {
   const project = ProjectItems.filter(
     (item) => item.slug === router.query.project_slug
   )[0];
-  return <div>ProjectShow: {project.title}</div>;
+  return <div>ProjectShow: {project?.title || 'not there'}</div>;
 };
 
 export default ProjectShow;
